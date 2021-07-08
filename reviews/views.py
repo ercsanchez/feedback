44450a3a5054e71370695a0e75d8740c2a37ed66
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-08 08:55:34 +0000

    136. revise thank_you view into a TemplateView

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 
@@ -17,6 +18,7 @@ class ReviewView(View):
 
     def post(self, request):
         form = ReviewForm(request.POST)
+
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/thank_you')
@@ -26,24 +28,10 @@ class ReviewView(View):
         })
 
 
-# function based view no longer used
-def review(request):
-    # form = None   # apparently python variables dont have block scoping, so form can be accessed outside of if-block
-    if request.method == "POST":
-        form = ReviewForm(request.POST)
+class ThankYouView(TemplateView):
+    template_name = 'reviews/thank_you.html'
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/thank_you')
-
-    # if request.method is not "POST" create a blank form
-    else:
-        form = ReviewForm()
-
-    return render(request, 'reviews/review.html', {
-        "form": form
-    })
-
-
-def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = "This works!"
+        return context
