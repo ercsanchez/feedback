0585c3e6c5c3347390fd2481cb344816b654e42c
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-08 05:20:10 +0000

    132. use a class based view for Review and wire to url

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,32 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 
 from .forms import ReviewForm
 
 # Create your views here.
 
 
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/thank_you')
+
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
+
+
+# function based view no longer used
 def review(request):
     # form = None   # apparently python variables dont have block scoping, so form can be accessed outside of if-block
     if request.method == "POST":
