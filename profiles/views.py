956f16e59d16382beb6e78b292246f51b9462f0a
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-09 02:11:23 +0000

    147. store uploaded files using built-in python functions - basic and naive approach

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
+
+def store_file(file):
+    with open('temp/image.jpg', 'wb+') as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+
 # Create your views here.
 
 
@@ -10,5 +17,5 @@ class CreateProfileView(View):
         return render(request, 'profiles/create_profile.html')
 
     def post(self, request):
-        print(request.FILES['image'])
+        store_file(request.FILES['image'])
         return HttpResponseRedirect('/profiles')
